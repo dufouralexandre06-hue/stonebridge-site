f590e675d73d9d016d766663c7bf8e3f1238f304
alexandre <dufouralexandre06@gmail.com>
2026-06-13 00:29:37 +0200

    Ajout Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import ScrollToTop from "@/components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Mandats from "./pages/Mandats";
 import Situations from "./pages/Situations";
@@ -32,6 +33,7 @@ const App = () => (
           </Routes>
         </BrowserRouter>
       </LanguageProvider>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
